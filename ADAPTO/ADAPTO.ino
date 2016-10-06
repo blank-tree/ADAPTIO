23b64529751d5eba3d0e568bdedd09aae9b350bd
Fernando Obieta <fernando.obieta@blanktree.ch>
2016-10-06 10:15:35 +0200

    initial commit with structure, readme and gitignore

---
+++ ADAPTO/ADAPTO.ino
@@ -0,0 +1,15 @@
+/**
+ * ADAPTI
+ * Input artefact for the physical computing module 2016 during the third semester «Interaction Design» studies at the Zurich University of the Arts
+ * @author: Fernando Obieta - https://blanktree.ch
+ * @date: 161016
+ * @version: 0.1
+ */
+
+void setup() {
+	
+}
+
+void loop() {
+	
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# ADAPTI
+## by Fernando Obieta - (https://blanktree.ch)blanktree.ch
+Input artefact for the physical computing module 2016 during the third semester «Interaction Design» studies at the Zurich University of the Arts
+author: Fernando Obieta - https://blanktree.ch
+@date: 161016
+@version: 0.1
