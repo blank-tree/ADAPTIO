8476722a51dc11780259dee5e02ea21ea82fbbfc
Fernando Obieta <fernando.obieta@blanktree.ch>
2016-10-11 10:26:04 +0200

    input readout

--- ADAPTI/ADAPTI.ino
+++ ADAPTI/ADAPTI.ino
@@ -2,13 +2,13 @@
  * ADAPTI
  * Input artefact for the physical computing module 2016 during the third semester «Interaction Design» studies at the Zurich University of the Arts
  * @author: Fernando Obieta - https://blanktree.ch
- * @date: 161006
- * @version: 0.1
+ * @date: 161011
+ * @version: 0.3
  * DO WHAT THE FUCK YOU WANT TO - PUBLIC LICENSE
  */
 
 //Constants
-const int PIN_PRESSURE = 0;
+
 
 // Variables
 int inputValue;
@@ -24,11 +24,13 @@ void setup() {
 	// Setup sending to switchr.io
 	sendSetup();
 
+	flexSetup();
+
 }
 
 void loop() {
 
-		
+	inputValue = flexLoop()
 
 	sendLoop(inputValue);
 }
--- ADAPTI/flex.ino
+++ ADAPTI/flex.ino
@@ -1,26 +1,51 @@
 /**
- * ADAPTI - pressure
- * Reads the pressure
+ * ADAPTI - flex
+ * Reads the flexing
  * @author: Fernando Obieta - https://blanktree.ch
- * @date: 161006
- * @version: 0.1
+ * @date: 161011
+ * @version: 0.3
  * DO WHAT THE FUCK YOU WANT TO - PUBLIC LICENSE
  */
 
 //Constants
-const int PIN_PRESSURE = 0;
+const int PINS_FLEX[4] = {0, 1, 2, 3};
+const int ARRAY_SIZE = 4;
 
 // Variables
 int inputValue;
+int flexValues[4];
 
 void flexSetup() {
 	
-	
+	inputValue = 0;
+
+	for(int i=0; i<4; i++){
+	    flexValues[i] = 0;
+	}
 
 }
 
-void flexLoop() {
+int flexLoop() {
 
-	
-	
+	int highestValue = 0;
+
+	for(int i=0; i<ARRAY_SIZE; i++){
+	    int currentReadout = analogRead(PINS_FLEX[i]);
+
+	    if (highestValue < currentReadout) {
+	    	highestValue = currentReadout;
+	    }
+	}
+
+	inputValue = inputValue * 0.8 + highestValue * 0.2;
+
+	if (inputValue > 1000) {
+		inputValue = 1000;
+	}
+
+	if (inputValue < 0) {
+		inputValue = 0;
+	}
+
+	return inputValue;	
 }
